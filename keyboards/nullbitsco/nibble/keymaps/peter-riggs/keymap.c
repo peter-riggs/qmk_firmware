ff1c130d656f99bf772bcb646f43ffda462f3748
peter-riggs <peterjriggs@gmail.com>
2022-08-14 18:13:27 +0800

    it finally works

--- keyboards/nullbitsco/nibble/keymaps/peter-riggs/keymap.c
+++ keyboards/nullbitsco/nibble/keymaps/peter-riggs/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 Jonathan Law, Jay Greco
+/* Copyright 2022 Peter Riggs
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,7 +13,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Original: j-inc's kyria keymap
  */
 #include QMK_KEYBOARD_H
 #include "animation_frames.h"
@@ -21,70 +20,26 @@
 enum layer_names {
   _BASE,
   _VIA1,
-  _VIA2,
-  _VIA3
 };
-
-#define KC_DISC_MUTE KC_F23
-#define KC_DISC_DEAF KC_F24
-
-enum custom_keycodes {
-  PROG = USER00,
-  DISC_MUTE,
-  DISC_DEAF,
-  SUPER_ALT_TAB,
-  _NUM_CUST_KCS,
-};
-
-// Macro variables
-bool is_alt_tab_active = false;
-uint16_t alt_tab_timer = 0;
-bool muted = false;
-bool deafened = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_all(
+  [_BASE] = LAYOUT_ansi(
                KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
     KC_MUTE,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
     KC_GESC,   KC_LCMD, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
-    _______,   KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
+    _______,   KC_LFST, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
     MO(_VIA1), KC_LCTL, KC_LOPT, KC_LCMD,                            KC_SPC,                    _______, KC_RCMD, KC_ROPT, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
-  [_VIA1] = LAYOUT_all(
+  [_VIA1] = LAYOUT_ansi(
               RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_SLEP,
     RGB_TOG,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
-    _______,  _______, _______, _______, _______, KC_CALC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,  _______, _______, _______, KC_CALC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______,  _______, _______, _______,                            _______,                   _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT
   ),
 
-  [_VIA2] = LAYOUT_all(
-              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
-    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______,  _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______, _______
-  ),
-
-  [_VIA3] = LAYOUT_all(
-              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
-    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______,  _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______, _______
-  ),
-
 };
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (clockwise) {
-        tap_code(KC_VOLU);
-    } else {
-        tap_code(KC_VOLD);
-    }
-    return true;
-}
 
 #ifdef OLED_ENABLE
 #define IDLE_FRAME_DURATION 200 // Idle animation iteration rate in ms
@@ -190,77 +145,64 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     #endif
 
-    switch(keycode) {
-        case PROG:
-          if (record->event.pressed) {
-            rgblight_disable_noeeprom();
-            #ifdef OLED_ENABLE
-            oled_off();
-            #endif
-            bootloader_jump();
-          }
-        break;
-
-        case DISC_MUTE:
-          if (record->event.pressed) {
-            tap_code(KC_DISC_MUTE);
-            if (!rgblight_is_enabled()) break;
-
-            if (muted) {
-              rgblight_enable_noeeprom();
-            } else {
-              rgblight_timer_disable();
-              uint8_t val = rgblight_get_val();
-              rgblight_sethsv_range(255, 255, val, 0, 1);
-            }
-            muted = !muted;
-          }
-        break;
-
-        case DISC_DEAF:
-          if (record->event.pressed) {
-            tap_code(KC_DISC_DEAF);
-            if (!rgblight_is_enabled()) break;
-
-            if (deafened) {
-              rgblight_enable_noeeprom();
-            } else {
-              rgblight_timer_disable();
-              uint8_t val = rgblight_get_val();
-              rgblight_sethsv_range(255, 255, val, 0, RGBLED_NUM-1);
-            }
-            deafened = !deafened;
-          }
-        break;
-
-        case SUPER_ALT_TAB:
-          if (record->event.pressed) {
-            if (!is_alt_tab_active) {
-              is_alt_tab_active = true;
-              register_code(KC_LALT);
-            }
-            alt_tab_timer = timer_read();
-            register_code(KC_TAB);
-          } else {
-            unregister_code(KC_TAB);
-          }
-          break;
-
-        default:
-        break;
-    }
-
     return true;
 }
 
-void matrix_scan_user(void) {
-  if (is_alt_tab_active) {
-    if (timer_elapsed(alt_tab_timer) > 1000) {
-      unregister_code(KC_LALT);
-      is_alt_tab_active = false;
-    }
-  }
+void matrix_init_user(void) {
+  // Initialize remote keyboard, if connected (see readme)
+  matrix_init_remote_kb();
 }
+
+void matrix_scan_user(void) {
+  // Scan and parse keystrokes from remote keyboard, if connected (see readme)
+  matrix_scan_remote_kb();
+}
+
+
+// RGB config, for changing RGB settings on non-VIA firmwares
+void change_RGB(bool clockwise) {
+    bool shift = get_mods() & MOD_MASK_SHIFT;
+    bool alt = get_mods() & MOD_MASK_ALT;
+    bool ctrl = get_mods() & MOD_MASK_CTRL;
+
+    if (clockwise) {
+        if (alt) {
+            rgblight_increase_hue();
+        } else if (ctrl) {
+            rgblight_increase_val();
+        } else if (shift) {
+            rgblight_increase_sat();
+        } else {
+            rgblight_step();
+        }
+
+  } else {
+      if (alt) {
+            rgblight_decrease_hue();
+        } else if (ctrl) {
+            rgblight_decrease_val();
+        } else if (shift) {
+            rgblight_decrease_sat();
+        } else {
+            rgblight_step_reverse();
+        }
+    }
+}
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (layer_state_is(1)) {
+        //change RGB settings
+        change_RGB(clockwise);
+        return true;
+    }
+    if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+    }
+    return true;
+}
+
 
 /*
 
